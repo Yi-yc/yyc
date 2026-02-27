2fdb6449a80d9376737f119de88c43d67a2e4543
“YYC” <yyc761306726@qq.com>
2026-02-27 16:54:22 +0800

    增加代码

---
+++ main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+void main()
+{
+    // Initialize the system
+    System_Init();
+
+    // Main loop
+    while (1)
+    {
+        // Perform main tasks
+        Main_Task();
+    }
+}
